7dc5aa22a8471d20167abae6d8069b6c6f6be599
Venki <18534venkanna@gmail.com>
2025-08-27 22:14:01 +0530

    Ranged Enemy

--- Build/Android/src/com/dynamicV/FinalTurn/OBBData.java
+++ Build/Android/src/com/dynamicV/FinalTurn/OBBData.java
@@ -18,8 +18,8 @@ mFileSize = fileSize;
 public static final XAPKFile[] xAPKS = {
 new XAPKFile(
 true, // true signifies a main file
-"33", // the version of the APK that the file was uploaded against
-494602000L // the length of the file in bytes
+"34", // the version of the APK that the file was uploaded against
+494613888L // the length of the file in bytes
 )
 };
 };
